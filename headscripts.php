54e2d6316d5c2261f820bffc41b7766553e3ea8e
RCid <rcid@myring.io>
2015-04-02 18:41:41 -0400

    indentation of original code (before we start refactoring)

--- headscripts.php
+++ headscripts.php
@@ -1,116 +1,58 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
+<!--!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"-->
 <html lang="en">
-
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>TeamPlayers</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Project X</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
 
-
-	<link href="css/bootstrap.css" rel="stylesheet">
-
-<!-- BARRA -->	
-
-	<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap-tooltip.js"></script>	
-	<script type="text/javascript" src="js/bootstrap-popover.js"></script>
-	<script type="text/javascript" src="js/bootstrap-modal.js"></script>
-	
-	
-<!-- PHOTOMOSAIC  LINIKS	 -->
-	<link rel="stylesheet" type="text/css" href="css/css_photomosaic/photoMosaic.screen.css" />
-	<script src="data/photoMosaic.data.js" type="text/javascript"></script>
-	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-	<script src="js/js_photomosaic/jquery.photoMosaic.js" type="text/javascript"></script>
-
-	<link rel="stylesheet" type="text/css" href="css/css_photomosaic/prettyPhoto.css" />
-	<script src="js/js_photomosaic/jquery.prettyPhoto.js" type="text/javascript"></script>
-	
-
+<!-- BARRA -->  
+    <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap-tooltip.js"></script>  
+    <script type="text/javascript" src="js/bootstrap-popover.js"></script>
+    <script type="text/javascript" src="js/bootstrap-modal.js"></script>
+    
+<!-- PHOTOMOSAIC  LINIKS     -->
+    <link rel="stylesheet" type="text/css" href="css/css_photomosaic/photoMosaic.screen.css" />
+    <script src="data/photoMosaic.data.js" type="text/javascript"></script>
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script src="js/js_photomosaic/jquery.photoMosaic.js" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="css/css_photomosaic/prettyPhoto.css" />
+    <script src="js/js_photomosaic/jquery.prettyPhoto.js" type="text/javascript"></script>
+    
 <!-- NIVO SLIDER START -->
-
-	 <link rel="stylesheet" href="css/css_nivo/default/default.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="css/css_nivo/default/default.css" type="text/css" media="screen" />
     <link rel="stylesheet" href="css/css_nivo/nivo-slider.css" type="text/css" media="screen" />
     <link rel="stylesheet" href="css/css_nivo/style.css" type="text/css" media="screen" />
     <script type="text/javascript" src="js/js_nivo/jquery.nivo.slider.js"></script>
 
-
 <!-- MY TEAM CSS-->
+     <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+                
+    <!-- <script type="text/javascript" src="js/css3-multi-column.js"></script> -->
+    
+    <script type='text/javascript'>
+        $(document).ready(function () {
+            if ($("[rel=tooltip]").length) {
+                $("[rel=tooltip]").tooltip();
+            }
+       });
+    </script> 
+    <script type='text/javascript'>
+        $(document).ready(function () {
+            if ($("[rel=popover]").length) {
+                $("[rel=popover]").popover({placement:'top'});
+            }  
+        });
+    </script>  
+    <script type='text/javascript'>
+        $(document).ready(function () {
+            if ($("[rel=modal]").length) {
+                $("[rel=modal]").modal({backdrop:'true'});
+            }  
+       });
+    </script>
 
-	 <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
-				
-	
-	<!-- <script type="text/javascript" src="js/css3-multi-column.js"></script> -->
-	
-	<script type='text/javascript'>
-     $(document).ready(function () {
-     if ($("[rel=tooltip]").length) {
-     $("[rel=tooltip]").tooltip();
-     }
-   });
-  </script>
-  
-  	<script type='text/javascript'>
-     $(document).ready(function () {
-     if ($("[rel=popover]").length) {
-     $("[rel=popover]").popover({placement:'top'})
-     ;
-     }
-     
-   });
-  </script>
-  
-   <script type='text/javascript'>
-     $(document).ready(function () {
-     if ($("[rel=modal]").length) {
-     $("[rel=modal]").modal({backdrop:'true'})
-     ;
-     }
-     
-   });
-  </script>
-  
+    <?php include('functions.php'); ?>
 
-<?php
-//CONNECTION TO DB
-
-define ("DB_NAME", "<yourDBname>");
-define ("DB_PASSWORD", "<yourPASS>");
-define ("DB_HOST", "<yourHOST>");
-define ("USERNAME", "<yourUSERNAME>");
-
-?>
-
-
-<?php
-
-$conn = @mysql_connect(DB_HOST,USERNAME,DB_PASSWORD);
-
-	//initialize for UTF8
-
-
-	mysql_query("", $conn);
-	//mysql_query("SET character_set_results=utf8", $conn);
-	//mb_language('uni');
-	//mb_internal_encoding('UTF-8');
-	mysql_select_db(DB_NAME, $conn);
-	//mysql_query("set names 'utf8'",$conn);
-	
-	
-	
-	if (!$conn) {
-	die('Could not connect: ' . mysql_error());
-}
-else{
-mysql_select_db(DB_NAME, $conn);
-//echo "coneccion OK <br/><br/><br/>";
-}
-?>
-
-
-
-
-
-
-<?php include('functions.php'); ?>
 </head>
--- thumbs.php
+++ thumbs.php
@@ -1,219 +1,111 @@
+<?php  
+    include('headscripts.php'); 
+?> 
+<body onLoad="init()"> 
+<?php  //CONNECTING WITH THIS YEAR DB
 
-	<?php  include('headscripts.php'); ?>
-	
-
-
-
-	
-<?	//CONNECTING WITH THIS YEAR DB
-
-if($_GET['id']){
-$id=$_GET['id'];
-}elseif($_POST['id']){
-$id=$_POST['id'];
-}
-
-
-$query=" SELECT * FROM TBL_print13 WHERE id = '".$id."'";
-$result = mysql_query($query);
-$row=mysql_fetch_array($result);
-
-
-
-
-	   //Capitalize						
-	$in=$row['name'];
-	$lowercaseTitle = strtolower($in);
-	$capitname= ucwords($lowercaseTitle);
-												
-					
-
+    if($_GET['id']){
+        $id=$_GET['id'];
+    }elseif($_POST['id']){
+        $id=$_POST['id'];
+    }
+    $query=" SELECT * FROM TBL_print13 WHERE id = '".$id."'";
+    $result = mysql_query($query);
+    $row=mysql_fetch_array($result);
+    //Capitalize                     
+    $in=$row['name'];
+    $lowercaseTitle = strtolower($in);
+    $capitname= ucwords($lowercaseTitle);
 ?>
-
-
-
-
-
-	<script>  $("a").tooltip(); // this will trigger a tooltip on all <a> elements 
-	</script>
-	
-	
-	<script type="text/javascript">
-	$(document).ready(function(){
-
-		$('#target').photoMosaic({
+    <script>  
+        $("a").tooltip(); // this will trigger a tooltip on all <a> elements 
+    </script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('#target').photoMosaic({             
+                columns: 3,              
+                width : 580,
+                padding: 1,
+                modal_name : 'prettyPhoto',
+                modal_ready_callback: function($pm) {
+                    $('a[rel^="prettyPhoto"]', $pm).prettyPhoto({
+                        theme: 'dark_rounded',
+                        overlay_gallery: false
+                    });
+                }       
+            });
             
-            columns: 3,
-            
-            width : 580,
-            padding: 1,
-            modal_name : 'prettyPhoto',
-            modal_ready_callback: function($pm) {
-                $('a[rel^="prettyPhoto"]', $pm).prettyPhoto({
-                    theme: 'dark_rounded',
-                    overlay_gallery: false
-                });
-            }		
-		});
+        });
+    </script>
+    <style type="text/css">
+        /* body{background:#ccc;} */
+        #container{width:450px; margin:5px auto 0px auto;}     
+    </style>
+    <div class="container">  
+<?php 
+    $backcolor="#FFE02B";
+?>     
+    <!-- ::::::::::::::::::::::ROW 1:::::::::::::::::::::: -->
+    <!-- ::::::::::::::::::::::ROW 2:::::::::::::::::::::: -->
+        <div class="row">   
+            <div class="span12" style="text-align: right; background-color:#FFF;">
+                <br/>
+<?php
+    $thiscity=$row['state'];        
+    //$query=" SELECT name,state,id,description,zip FROM TBL_print13 WHERE exist=5 AND service=0 ORDER BY state";
+    $query=" SELECT name,state,id,description,zip,room,occu,chil,service FROM TBL_print14 WHERE exist=5 AND status_agreement=2 ORDER BY state";
+?>
+                <div style="position:fixed; top:0px; left:0px; width:90px; padding:5px 50px; background-color:white; 
+                border-bottom-style:solid; border-bottom:1px #CCC; font-size:0.7em; ">
+                    <span class="lead">Team Players Content</span>
+                    <hr/>
         
-	});
-</script>
-
-
-<style type="text/css">
-	/* body{background:#ccc;} */
-	#container{width:450px; margin:5px auto 0px auto;}
-
-	
-</style>
-
-
-
-
-    <div class="container">
-    
- <?php 
-$backcolor="#FFE02B";
-
-
+<?php
+    $prev1='';
+    $result = mysql_query($query);
+    while($row=mysql_fetch_array($result)){
+        $state=$row['state'];
+        if($state==$prev1){}else{
+            echo "<a href='#$state'> $state <br/> </a>";
+            $prev1=$state;
+        }
+    }
+?>
+                </div>  
+                <br/><br/>
+<?php
+    $prev2='';
+    $result = mysql_query($query);
+    while($row=mysql_fetch_array($result)){  
+        $name=$row['name'];
+        $state=$row['state'];
+        $id=$row['id'];
+        $description=$row['description'];
+        $zip=$row['zip'];
+        $service=$row['service'];
+        $room=$row['room'];
+        $occu=$row['occu'];
+        $chil=$row['chil'];
+                
+                
+        if($state==$prev2){echo '**';}else{ 
 ?>  
-    
-<!-- ::::::::::::::::::::::ROW 1:::::::::::::::::::::: -->
-
-
-      				
-
-
-<!-- ::::::::::::::::::::::ROW 2:::::::::::::::::::::: -->
-
-
-      <div class="row">   
-
-      		
-      		
-      		
-      		
-      		
-      		<div class="span12" style="text-align: right; background-color:#FFF;">
-      		
-		      	<br/>
-		      		
-		      		 
-		      		
-		      		
-		      		<?php
-		      		
-		      		$thiscity=$row['state'];
-		      		
-			//$query=" SELECT name,state,id,description,zip FROM TBL_print13 WHERE exist=5 AND service=0 ORDER BY state";
-			$query=" SELECT name,state,id,description,zip,room,occu,chil,service FROM TBL_print14 WHERE exist=5 AND status_agreement=2 ORDER BY state";
-			
-			
-			
-			?>
-			
-			<div style="position:fixed; top:0px; left:0px; width:90px; padding:5px 50px; background-color:white; 
-			border-bottom-style:solid; border-bottom:1px #CCC; font-size:0.7em; ">
-				<span class="lead">Team Players Content</span><hr/>
-		      		
-			<?php
-			
-				$prev1='';
-				
-						$result = mysql_query($query);
-
-
-				while($row=mysql_fetch_array($result)){
-				
-						
-						$state=$row['state'];
-						
-						
-						
-							
-							if($state==$prev1){}else{
-							echo "<a href='#$state'> $state <br/> </a>";
-							
-							
-							$prev1=$state;
-							}
-						
-						}
-
-			?>
-			</div>
-			
-			<br/><br/>
-			
-			<?php
-				$prev2='';
-						
-						$result = mysql_query($query);
-						
-						
-				while($row=mysql_fetch_array($result)){
-				
-				$name=$row['name'];
-				$state=$row['state'];
-				$id=$row['id'];
-				$description=$row['description'];
-				$zip=$row['zip'];
-				$service=$row['service'];
-				
-				$room=$row['room'];
-				$occu=$row['occu'];
-				$chil=$row['chil'];
-				
-				
-				if($state==$prev2){echo '**';}else{ ?>	
-					<div style="background-color:orange; padding:0px 4px;">
-						<p><a id="<?php echo $state;?>"><?php echo $state;?></a></p>
-		      		</div>
-		      		
-				<?php $prev2=$state; }
-				
-				
-					/* other2($name,$state,"thumbs/s_$id.jpg",$id); */
-					test1($name,$state,$id,$description,$zip,$room,$occu,$chil,$service);
-					
-					
-						
-						};
-		      		
-		      		
-		      		
-		      		?>
-		      				
-		      		
-		      		
-		      		
-      		</div>  
-      		 
-      		
-       
-      
-
-       
-      </div>
-    
-
-    
-    
-    <!-- ::::::::::::::::::::::ROW 3:::::::::::::::::::::: -->
-
-
-
-
-
-
-
-
- </div> <!-- final DIV -->
-      
-
-
-
-<!--<script src="http://blueimp.github.com/cdn/js/bootstrap.min.js"></script>-->
-<script src="http://blueimp.github.com/JavaScript-Load-Image/load-image.min.js"></script>
-<script src="js/bootstrap-image-gallery.min.js"></script>
+                <div style="background-color:orange; padding:0px 4px;">
+                    <p><a id="<?php echo $state;?>"><?php echo $state;?></a></p>
+                </div>      
+<?php 
+        $prev2=$state; 
+        }
+        /* other2($name,$state,"thumbs/s_$id.jpg",$id); */
+        test1($name,$state,$id,$description,$zip,$room,$occu,$chil,$service);
+    };
+?>
+            </div>  
+        </div>
+        <!-- ::::::::::::::::::::::ROW 3:::::::::::::::::::::: -->
+    </div> <!-- final DIV -->
+    <!--<script src="http://blueimp.github.com/cdn/js/bootstrap.min.js"></script>-->
+    <script src="http://blueimp.github.com/JavaScript-Load-Image/load-image.min.js"></script>
+    <script src="js/bootstrap-image-gallery.min.js"></script>
+</body>
       
